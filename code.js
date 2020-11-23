f2ab1ff816c4a444a1a74aef18e2cf34a6f8832f
antoniaciobanu <71286880+antoniaciobanu@users.noreply.github.com>
2020-11-23 17:38:13 +0000

    Add files via upload

---
+++ code.js
@@ -0,0 +1,183 @@
+let slideIndex = 0;
+let slider = document.querySelector(".slider");
+let slides = document.querySelector(".slides");
+let slide = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dots span")
+
+showslide();
+
+function plusslide(position) {
+    slideIndex += position;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+}
+
+function currentslide(index) {
+    if (index > slide.length) {
+        index = 1;
+    }
+    else if (index < 1) {
+        index = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${index - 1}00%`;
+    dots[index - 1].classList.add("dot-active");
+
+    slideIndex = index;
+}
+
+function showslide() {
+    slideIndex++;
+
+    if (slideIndex > slide.length) {
+        slideIndex = 1;
+    }
+    else if (slideIndex < 1) {
+        slideIndex = slide.length;
+    }
+
+    // Defaultly active class is removed from all dots
+    for (let i = 0; i < dots.length; i++) {
+        const element = dots[i];
+        element.classList.remove("dot-active");
+    }
+
+    slides.style.left = `-${slideIndex - 1}00%`;
+    dots[slideIndex - 1].classList.add("dot-active");
+}
+
+
+let interval = setInterval(()=> {
+    showslide();
+} , 3000);   // Change every image after 4 seconds
+
+slider.addEventListener("mouseover" , ()=> {
+    clearInterval(interval);     // onHover Stop Changing every image after 4 seconds 
+});
+
+slider.addEventListener("mouseover" , ()=> {
+    interval = setInterval(()=> {
+        showslide();   
+    } , 3000);    // on mouseout from slide then again start Changing every image after 4  seconds 
+});
+
+
+
+
+/////// Card Slider//////
+
+function updateSliderArrowsStatus(
+    cardsContainer,
+    containerWidth,
+    cardCount,
+    cardWidth
+  ) {
+    if (
+      $(cardsContainer).scrollLeft() + containerWidth <
+      cardCount * cardWidth + 15
+    ) {
+      $("#slide-right-container").addClass("active");
+    } else {
+      $("#slide-right-container").removeClass("active");
+    }
+    if ($(cardsContainer).scrollLeft() > 0) {
+      $("#slide-left-container").addClass("active");
+    } else {
+      $("#slide-left-container").removeClass("active");
+    }
+  }
+  $(function() {
+    // Scroll products' slider left/right
+    let div = $("#cards-container");
+    let cardCount = $(div)
+      .find(".cards")
+      .children(".card").length;
+    let speed = 1000;
+    let containerWidth = $(".container").width();
+    let cardWidth = 300;
+  
+    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+  
+    //Remove scrollbars
+    $("#slide-right-container").click(function(e) {
+      if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
+        $(div).animate(
+          {
+            scrollLeft: $(div).scrollLeft() + cardWidth
+          },
+          {
+            duration: speed,
+            complete: function() {
+              setTimeout(
+                updateSliderArrowsStatus(
+                  div,
+                  containerWidth,
+                  cardCount,
+                  cardWidth
+                ),
+                1005
+              );
+            }
+          }
+        );
+      }
+      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+    });
+    $("#slide-left-container").click(function(e) {
+      if ($(div).scrollLeft() + containerWidth > containerWidth) {
+        $(div).animate(
+          {
+            scrollLeft: "-=" + cardWidth
+          },
+          {
+            duration: speed,
+            complete: function() {
+              setTimeout(
+                updateSliderArrowsStatus(
+                  div,
+                  containerWidth,
+                  cardCount,
+                  cardWidth
+                ),
+                1005
+              );
+            }
+          }
+        );
+      }
+      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+    });
+  
+    // If resize action ocurred then update the container width value
+    $(window).resize(function() {
+      try {
+        containerWidth = $("#cards-container").width();
+        updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+      } catch (error) {
+        console.log(
+          `Error occured while trying to get updated slider container width: 
+              ${error}`
+        );
+      }
+    });
+  });
